8d11ada579e2dce71303230577786538386c12fc
Srijan Aryal <srizan.aryal31@gmail.com>
2020-01-19 15:49:40 -0500

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import header from "./img/blood-297828_640.png";
 
 function App() {
   return (
     <Router>
-      <div className="App"></div>
+      <div className="App">
+        <div className="container-fluid">
+          <div>Blood Bank</div>
+        </div>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light red">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <Link to={"/"} className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to={"/"} className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to={"/"} className="nav-link">
+                  Home
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </Router>
   );
 }
